cce9c476666624efca7879d5da56bec230b7ae32
tihuan <TimmyHuang0404@Gmail.com>
2016-01-19 20:46:25 -0800

    demo done

--- main.js
+++ main.js
@@ -8,11 +8,12 @@ angular
 angular
   .module('app')
   .directive('output', [
+    '$timeout',
     'rx',
     output
   ]);
 
-function output(rx) {
+function output($timeout, rx) {
   var directive = {
     restrict: 'E',
     template: template,
@@ -24,36 +25,73 @@ function output(rx) {
   function template(ele, attrs) {
     return [
       '<h1>RxJS ROCKS!</h1>',
-      '<div ng-repeat="itemA in streamA">',
-        '{{itemA}}',
-      '</div>',
+      '{{collectionA}}',
       '<br>',
-      '<div ng-repeat="itemB in streamB">',
-        '{{itemB}}',
-      '</div>',
+      '{{collectionB}}',
       '<br>',
-      '<div ng-repeat="combinedItem in combinedStream">',
-        '{{combinedItem}}',
-      '</div>'
+      '{{collectionC}}',
     ].join('\n');
   }
 
   function link(scope, ele, attrs) {
+    var subA, subB, subC
     var source = rx.Observable.interval(1000);
-    scope.streamA = []
-    scope.streamB = [4, 5, 6];
+    var hot = source.publish();
+    var streamA = new rx.Subject();
+    var streamB = new rx.Subject();
+    var combinedStream;
+    scope.collectionA = [];
+    scope.collectionB = [];
+    scope.collectionC = [];
 
-    source
+    hot
+      .subscribe(function(value) {
+        streamA.onNext(value + 1);
+      });
+
+    $timeout(function() {
+      hot
+        .subscribe(function(value) {
+          streamB.onNext(value + 1);
+        });
+    }, 5 * 1000)
+
+    combinedStream = rx.Observable
+      .zip(
+        streamA,
+        streamB,
+        function(itemA, itemB) {
+          return 'A: ' + itemA + ' ' + 'B: ' + itemB;
+        }
+      );
+
+    subA = streamA
+      .safeApply(scope, function(itemA) {
+        scope.collectionA.push(itemA);
+      })
+      .subscribe()
+
+    subB = streamB
+      .safeApply(scope, function(itemB) {
+        scope.collectionB.push(itemB);
+      })
+      .subscribe()
+
+    subC = combinedStream
       .safeApply(
         scope,
-        function(value) {
-          console.log('value here:');
-          console.log(value);
-          scope.streamA.push(value);
-          console.log(scope.streamA);
-        })
-      .subscribe();
+        function(result) {
+          scope.collectionC.push(result);
+        }
+      )
+      .subscribe()
 
-    scope.combinedStream = ['a', 'b', 'c'];
+    $timeout(function() {
+      subA.dispose();
+      subB.dispose();
+      subC.dispose();
+    }, 15 * 1000)
+
+    hot.connect();
   }
 }
